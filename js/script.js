383b334e8cf95bca28548214309815c50bd524f7
namiqmamedov <namigmm@code.edu.az>
2023-04-14 14:19:22 +0400

    Updated course detail page

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ hamburger.onclick = function () {
     navBar = document.querySelector(".main-text")
     navBar.classList.toggle("active");
 }
+
+
+/** code by webdevtrick ( https://webdevtrick.com ) **/
+$(function() {
+    $('.list-heading').on('click', function(e) {
+      e.preventDefault();
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $(this).next()
+        .stop()
+        .slideUp(300);
+      } else {
+        $(this).addClass('active');
+        $(this).next()
+        .stop()
+        .slideDown(300);
+      }
+    });
+  });
