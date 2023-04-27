11b72a726d65e07961b2c4a78b5fd509c13a94d4
namiqmamedov <namigmm@code.edu.az>
2023-04-27 14:15:48 +0400

    Updated account detail page & created error page

--- js/script.js
+++ js/script.js
@@ -1,54 +1,27 @@
-$(document).ready(function() {  
-  $('.account-content ul li a').click(function(){  
-    $('.account-content li a').removeClass("active");  
-    $(this).addClass("active");  
-});  
-});  
 
+const viewBtn = document.querySelector(".view-modal"),
+popup = document.querySelector(".popup"),
+close = popup.querySelector(".close"),
+field = popup.querySelector(".field"),
+input = field.querySelector("input"),
+copy = field.querySelector("button");
 
-
-hamburger = document.querySelector(".hamburger")
-
-hamburger.onclick = function () {
-    navBar = document.querySelector(".main-text")
-    navBar.classList.toggle("active");
+viewBtn.onclick = ()=>{
+  popup.classList.toggle("show");
+}
+close.onclick = ()=>{
+  viewBtn.click();
 }
 
-// nav box shadow adding in scroll
-
-var navBar = document.getElementById('header-main')
-
-window.addEventListener('scroll', () => {
-  if(window.scrollY > 100){
-    navBar.classList.add('active')
+copy.onclick = ()=>{
+  input.select(); //select input value
+  if(document.execCommand("copy")){ //if the selected text copy
+    field.classList.add("active");
+    copy.innerText = "Copied";
+    setTimeout(()=>{
+      window.getSelection().removeAllRanges(); //remove selection from document
+      field.classList.remove("active");
+      copy.innerText = "Copy";
+    }, 3000);
   }
-  else{
-    navBar.classList.remove('active')
-  }
-})
-
-// animation login page 
-
-
-var password = document.getElementById('inputBox');
-var icon = document.getElementById('toggle-password');
-
-// input show hide button
-
-myPass = () => {
-   if(password.type == 'password'){
-      password.setAttribute('type','text');
-      icon.classList.remove('fa-eye')
-      icon.classList.add('fa-eye-slash');
-   }
-   else{
-      icon.classList.add('fa-eye');
-      icon.classList.remove('fa-eye-slash');
-      password.setAttribute('type','password');
-   }
 }
-
-icon.addEventListener('click', myPass)
-
-
-
